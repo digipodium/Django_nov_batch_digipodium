64ca1ac309f69c6b201e982853329278d3533f97
zaid kamil <xaidmetamorphos@gmail.com>
2021-12-07 20:27:29 +0530

    model and view

--- webbasics/app/models.py
+++ webbasics/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    # title, subject, content, date, is_published
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+# create a model for Website News
+# headline
+# date
+# details
+# author
+# views -> IntegerField
+
--- webbasics/app/views.py
+++ webbasics/app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html", context={})
+    articles = Article.objects.all()
+    # news = ...
+    ctx = {
+        'articles':articles,
+        #'news':news,
+    }
+    return render(request, "index.html", context=ctx)
 
 def contact(request):
     return render(request, "contact.html", context={})
