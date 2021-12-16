06487d8382653f301dafc57930452bd34ff93f06
zaid kamil <xaidmetamorphos@gmail.com>
2021-12-16 19:57:46 +0530

    new website

---
+++ webadv/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
+    path('about/', TemplateView.as_view(template_name='about.html'), name="about"),
+    path('terms/', TemplateView.as_view(template_name='tnc.html'),name="terms"), 
+]
