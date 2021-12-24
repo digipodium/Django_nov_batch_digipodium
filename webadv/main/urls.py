2658241ce476469c33704347950a88c24e9684bb
zaid kamil <xaidmetamorphos@gmail.com>
2021-12-24 19:55:00 +0530

    display content

--- webadv/main/urls.py
+++ webadv/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/', TemplateView.as_view(template_name='about.html'), name="about"),
     path('terms/', TemplateView.as_view(template_name='tnc.html'),name="terms"), 
+    path('article/<int:id>/',views.article_detail, name='article'),
 ]
--- webadv/main/views.py
+++ webadv/main/views.py
@@ -4,11 +4,26 @@ from .models import *
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    articles = Article.objects.all()
+    filter_cat = request.GET.get('c',0)
+    filter_author = request.GET.get('a')
+    if filter_cat:
+        articles = Article.objects.filter(category = filter_cat)
+    elif filter_author:
+        articles = Article.objects.filter(user = filter_author)
+    else:
+        articles = Article.objects.all()
 
     ctx = {
         'articles': articles,
         'categories' : categories,
+        'fc': int(filter_cat),
     }
 
     return render(request, 'index.html', ctx)
+
+def article_detail(request,id):
+    article = Article.objects.get(id=id)
+    ctx ={
+        'data': article
+    }
+    return render(request,'article_details.html',ctx)
