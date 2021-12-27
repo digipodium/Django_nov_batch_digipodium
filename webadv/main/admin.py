04d0854975973688780e8082a868767102a4fa8a
zaid kamil <xaidmetamorphos@gmail.com>
2021-12-27 20:01:45 +0530

    admin dashboard

--- webadv/main/admin.py
+++ webadv/main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from .models import *
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Article)
+class CategoryAdmin(ModelAdmin):
+    list_display = ('name','id','icon')
+
+class ArticleAdmin(ModelAdmin):
+    list_display = ('title','category','user','date')
+    search_fields = ('category__name','title')
+    list_filter = ('date','user')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
