10b79374ca21a4749bb6d7f2dd5e5723796388bd
zaid kamil <xaidmetamorphos@gmail.com>
2022-01-05 19:14:26 +0530

    auth

--- webadv/auth/forms.py
+++ webadv/auth/forms.py
@@ -19,3 +19,44 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Your Username',
+                'class':'form-control'
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder':'Your Email',
+                'class' : 'form-control'
+            }
+        )
+    )
+
+    password1 =  forms.CharField(
+        widget= forms.PasswordInput(
+            attrs = {
+                'placeholder' : 'Your Password',
+                'class' : 'form-control'
+            }
+        )
+    )
+
+    password2 =  forms.CharField(
+        widget= forms.PasswordInput(
+            attrs = {
+                'placeholder' : 'Cofirm Password',
+                'class' : 'form-control'
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
--- webadv/auth/views.py
+++ webadv/auth/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def login_view(request):
     form = LoginForm(request.POST or None)
-    msg = None
     if request.method == "POST":
         if form.is_valid():
             username = form.cleaned_data.get('username')
@@ -13,16 +13,31 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, "Login Successfull")
                 return redirect('index')
             else:
-                msg = "Invalid Credentials"
+                messages.error(request,"Invalid Credentials")
         else:
-            msg = "Error Validating the form"
+            messages.error(request,"Error Validating the Form")
     ctx = {
         'form':form,
-        'msg' : msg,
     }
     return render(request, 'accounts/login.html',ctx)
 
+
 def register_view(request):
-    pass
+    if request.method == "POST":
+        form = RegisterForm(request.POST) # filled form
+        if form.is_valid():
+            form.save() # db me user created
+            messages.success(request, "Account Created Successfully")
+            return redirect('login')
+        else:
+           messages.error(request, "Invalid form details")
+    else:
+        form = RegisterForm() # blank form
+    ctx = {
+        'form': form
+    }
+
+    return render(request, 'accounts/register.html', ctx)
